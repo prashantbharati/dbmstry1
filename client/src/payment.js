d2a974129348d30596f235c0cd2fbba438c12974
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-05-02 08:25:51 +0530

    new form

--- client/src/payment.js
+++ client/src/payment.js
@@ -1,7 +1,39 @@
 import React from "react";
+import Form4 from "./Form/Form4.js";
+import Nav from "./Nav/Nav.js";
+const Payment = ({ oid, setoid }) => {
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Nav />
+      </div>
 
-const payment = () => {
-  return <div>payment</div>;
+      <div
+        style={{
+          display: "flex",
+          height: "100vh",
+          justifyContent: "center",
+          alignItems: "center",
+          textAlign: "center",
+          background:
+            "linear-gradient(90deg,rgb(227, 16, 255) 0%,rgb(255, 252, 100) 100%)",
+        }}
+      >
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+
+        <Form4 oid={oid} setoid={setoid} />
+      </div>
+    </>
+  );
 };
-
-export default payment;
+export default Payment;
